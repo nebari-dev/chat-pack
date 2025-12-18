dfc403e80f5dcab6538986ab8b0340c3a033b221
Chris Colbert <sccolbert@gmail.com>
2025-12-18 17:47:52 -0600

    use container queries for the metrics chart page

--- src/components/metrics/metrics.tsx
+++ src/components/metrics/metrics.tsx
@@ -40,14 +40,11 @@ import {
 
 /**
  * A React component that renders the metrics chart page.
- *
- * TODO enable tailwind container queries for this component instead
- * of using the default screen query.
  */
 export
 function Metrics(): ReactNode {
   return (
-    <main className='grow flex flex-col'>
+    <main className='grow flex flex-col @container'>
       <div className={ cn(
         'px-4 py-2 flex flex-row gap-4 items-center justify-between',
         'border-b border-bd-neutral-default') }>
@@ -58,7 +55,7 @@ function Metrics(): ReactNode {
       </div>
       <div className={ cn(
         'p-4 grow min-h-0 overflow-y-auto grid gap-4 auto-rows-[1fr]',
-        'grid-cols-1 lg:grid-cols-2 xl:grid-cols-3') }>
+        'grid-cols-1 @3xl:grid-cols-2 @6xl:grid-cols-3') }>
         <TokensChart />
         <UsersChart />
         <AgentRunsChart />
