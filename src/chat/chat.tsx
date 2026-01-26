6cb2e6d62242334f077f4878a36df60fa4c3cfb6
Chris Colbert <sccolbert@gmail.com>
2026-01-26 13:00:09 -0600

    implement scroll to bottom for the chat

--- src/chat/chat.tsx
+++ src/chat/chat.tsx
@@ -2,7 +2,7 @@
 | Copyright (c) 2025-present, OpenTeams Inc.
 |----------------------------------------------------------------------------*/
 import type {
-  ReactNode
+  PropsWithChildren, ReactNode
 } from 'react';
 
 import {
@@ -21,6 +21,10 @@ import {
   Header
 } from './header';
 
+import {
+  useScrollToBottom
+} from './usescrolltobottom';
+
 
 /**
  * A component that renders the chat panel.
@@ -30,12 +34,39 @@ function Chat(): ReactNode {
   return (
     <main className='grow flex flex-col'>
       <Header />
-      <div className='px-4 grow min-h-0 flex flex-col gap-6 overflow-y-auto'>
-        <ChatRuntimeProvider>
+      <ChatRuntimeProvider>
+        <Private.Viewport>
           <ChatOutput />
           <ChatInput />
-        </ChatRuntimeProvider>
-      </div>
+        </Private.Viewport>
+      </ChatRuntimeProvider>
     </main>
   );
+}
+
+
+/**
+ * The namespace for the module implementation details.
+ */
+namespace Private {
+  /**
+   * A react component that renders the scroll viewport for the chat.
+   */
+  export
+  function Viewport(props: PropsWithChildren): ReactNode {
+    // Extract the props.
+    const { children } = props;
+
+    // Fetch the auto-scroll ref from the hook.
+    const ref = useScrollToBottom();
+
+    // Return the rendered component.
+    return (
+      <div
+        ref={ ref }
+        className='px-4 grow min-h-0 flex flex-col gap-6 overflow-y-auto'>
+        { children }
+      </div>
+    );
+  }
 }
