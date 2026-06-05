3c348e25d3e778f15ccb3d22a5c7e181694ef02b
Johnny Bouder <jbouder@openteams.com>
2026-06-05 09:51:09 -0400

    Cleanup biome scripts. Includes updates. Use ci in workflow.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,8 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
-
+import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -13,25 +12,25 @@ export default defineConfig(({ mode }) => {
         target: 'react',
         semicolons: true,
         routeTreeFileHeader: [
-          "// @ts-nocheck",
-          "// noinspection JSUnusedGlobalSymbols",
+          '// @ts-nocheck',
+          '// noinspection JSUnusedGlobalSymbols',
         ],
       }),
       tailwindcss(),
-      react()
+      react(),
     ],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src')
-      }
+        '@': path.resolve(__dirname, './src'),
+      },
     },
     server: {
       proxy: {
         '/api': {
           target: env.VITE_API_URL,
-          changeOrigin: true
-        }
-      }
-    }
+          changeOrigin: true,
+        },
+      },
+    },
   };
 });
